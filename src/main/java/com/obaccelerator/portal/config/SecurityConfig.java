21675e8d7449503e793dcb2c11d3e8608f09a28a
ruben <ruben.van.loen@ing.com>
2020-05-03 11:18:18 +0200

    Ditched comments and unused dependencies

--- src/main/java/com/obaccelerator/portal/config/SecurityConfig.java
+++ src/main/java/com/obaccelerator/portal/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.obaccelerator.portal.config;
 
-import com.obaccelerator.portal.authentication.cognito.CognitoService;
 import com.obaccelerator.portal.authentication.spring.SpringSessionAuthenticationFilter;
 import com.obaccelerator.portal.portaluser.PortalUserService;
-import com.obaccelerator.portal.session.FirstTimeSessionService;
 import com.obaccelerator.portal.shared.session.SessionService;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -17,47 +15,19 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SessionService sessionService;
-    private final CognitoService cognitoService;
     private final PortalUserService portalUserService;
-    private final FirstTimeSessionService firstTimeSessionService;
 
-    public SecurityConfig(SessionService sessionService, CognitoService cognitoService, PortalUserService portalUserService, FirstTimeSessionService firstTimeSessionService) {
+    public SecurityConfig(SessionService sessionService, PortalUserService portalUserService) {
         this.sessionService = sessionService;
-        this.cognitoService = cognitoService;
         this.portalUserService = portalUserService;
-        this.firstTimeSessionService = firstTimeSessionService;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-/*
-        This may work :
-        - Create a OncePerRequestFilter that is limited to  a particular URL using the FilterRegistrationBean for initial authentication
-                - Use a second OncePerRequestFilter that fires always for per-request authentication
-
-            In both cases use SecurityContextHolder.getContext().setAuthentication() to set the authentication, either ORGANIZATION_ADMIN OR ANONYMOUS
-
-                AbstractPreAuthenticatedProcessingFilter cannot be tied to a specific URL. I don't see why I would use this for per-request authentication
-        AbstractAuthenticationProcessingFilter can be tied to a URL, but insists we redirect somewhere after authentication. It doesn't seem to be intended for REST
-*/
-
-/*
-        ObaCognitoAuthFilter obaAuthFilter = new ObaCognitoAuthFilter(SESSION_URL, cognitoService, portalUserService, firstTimeSessionService, sessionService);
-        obaAuthFilter.setAllowSessionCreation(false);
-        obaAuthFilter.setAuthenticationDetailsSource(new ObaAuthenticationDetailsSource(portalUserService, cognitoService));
-        obaAuthFilter.setAuthenticationSuccessHandler(new ForwardAuthenticationSuccessHandler(SESSION_URL));
-        obaAuthFilter.setAuthenticationManager(new AuthenticationManager() {
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                return null;
-            }
-        });
-*/
-
         http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
